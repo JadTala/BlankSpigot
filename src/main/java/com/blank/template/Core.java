5faeefc68ef343e6e8b058f0f11ff483c59b6898
Jad Tala <JadTala1621@gmail.com>
2021-11-26 15:47:37 +0100

    (Plugin) Initial template

---
+++ src/main/java/com/blank/template/Core.java
@@ -0,0 +1,13 @@
+package com.blank.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
